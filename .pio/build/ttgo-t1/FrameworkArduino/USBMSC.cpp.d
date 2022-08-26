06cfd60e5be1065df75c2bfd3eb0b982e05455bd
mdiazfer <mdiazfer@gmail.com>
2022-08-26 18:42:56 +0200

    Added: - .gitignore - .pio/* - .vscode/*

---
+++ .pio/build/ttgo-t1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/ttgo-t1/FrameworkArduino/USBMSC.cpp.o: \
+ /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
---
+++ .pio/build/ttgo-t1/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/ttgo-t1/FrameworkArduino/firmware_msc_fat.c.o: \
+ /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
