a78b80fc6c580148d03fa8f4d1e185748021b6f5
mdiazfer <mdiazfer@gmail.com>
2022-09-02 18:13:23 +0200

    deleted files

--- .pio/build/ttgo-t1/FrameworkArduino/esp32-hal-tinyusb.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/ttgo-t1/FrameworkArduino/esp32-hal-tinyusb.c.o: \
- /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
- /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
--- .pio/build/ttgo-t1/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/ttgo-t1/FrameworkArduino/libb64/cdecode.c.o: \
- /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /Users/madiazfer/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
