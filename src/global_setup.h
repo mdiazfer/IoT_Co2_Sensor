00db594bc889c1ff114452eb558939ba5d672ebd
mdiazfer <mdiazfer@gmail.com>
2022-09-02 18:09:05 +0200

    global_setup.h

--- src/global_setup.h
+++ src/global_setup.h
@@ -3,9 +3,10 @@
     No user modification is required in her
 */
 
+#include "user_setup.h"
+
 //Global board stuff
 #define ERROR_LED 25
-
 
 //Error stuff
 #define NO_ERROR                0x00
@@ -14,8 +15,35 @@
 #define ERROR_BUTTONS_SETUP     0x03
 #define ERROR_WIFI_SETUP        0x04
 #define ERROR_BLE_SETUP         0x05
-
-
+#define ERROR_SSID_CONNECTION   0x06
 
 //Display stuff
 #define TFT_BLACK 0x0000 // black
+
+//WiFi stuff
+#define MAX_CONNECTION_ATTEMPTS 16
+
+//Global stuff
+#ifdef _DECLAREGLOBALPARAMETERS_
+  bool logsOn = true;         //Whether enable or not logs on the seriaml line [TRUE | FALSE]
+
+  #ifndef _WIFINETWORKINFO_
+    typedef struct {
+      uint8_t networkItem;
+      String ssid;
+      uint8_t encryptionType;
+      int32_t RSSI;
+      uint8_t* BSSID;
+      int32_t channel;
+    } wifiNetworkInfo;  //Struct to store WiFi parameters
+    
+    #define _WIFINETWORKINFO_ 
+  #endif
+
+  wifiNetworkInfo wifiNet;
+  
+  #undef _DECLAREGLOBALPARAMETERS_
+#endif
+
+
+
